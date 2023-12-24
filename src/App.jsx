1ce1af6aad9e85b9f53979a20fae2742287a610a
Najmul shaon <najmul.nh.shaon@gmail.com>
2023-12-24 23:33:52 +0600

    create react file

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Test from "./Test";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h3>Test</h3>
+        <Test name="ass"></Test>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Test.jsx
@@ -0,0 +1,8 @@
+export default function Test(props) {
+  return (
+    <div>
+      <h4>test v2</h4>
+      <p>Para : {props}</p>
+    </div>
+  );
+}
